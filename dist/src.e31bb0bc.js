a9e8511864b4e89c8c8efca95a862db1986eca73
Gonzalo Alcina <gonzalo@teamthunderfoot.com>
2023-01-23 10:29:15 +0100

    update

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -201,7 +201,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50055" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49334" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
